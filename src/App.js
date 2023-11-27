c7ffe3d1e80b4e2dbe8bcdcbcf55c901e7e4ef2e
shrikant-sharma-707 <151537673+shrikant-sharma-707@users.noreply.github.com>
2023-11-27 14:59:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,64 +3,35 @@ import './App.css';
 
 function App() {
   const [students, setStudents] = useState([]);
-  const [student, setStudent] = useState([]);
   useEffect(() => {
-    //    fetch('https://jsonplaceholder.typicode.com/photos')
-        fetch('http://localhost:8080/rest/api')
+    fetch(`http://localhost:8080/findAllStudent`)
       .then((response) => response.json())
       .then((json) => setStudents(json));
-  }, []);
-  useEffect(() => {
-    //    fetch('https://jsonplaceholder.typicode.com/photos')
-        fetch('http://localhost:8080/rest/api/1')
-      .then((response) => response.json())
-      .then((json) => setStudent(json));
   }, []);
 
   return (
     <div>
-      <h1>students</h1>
+      <h1>student List</h1>
       <table>
         <thead>
           <tr>
-            <th>studentId</th>
+            <th>rollNo</th>
             <th>firstName</th>
             <th>lastName</th>
             <th>email</th>
-            <th>phone</th>
+            <th>phoneNumber</th>
           </tr>
         </thead>
         <tbody>
-          {students.map((photo) => (
-            <tr key={photo.studentId}>
-              <td>{photo.studentId}</td>
-              <td>{photo.firstName}</td>
-              <td>{photo.lastName}</td>
-              <td>{photo.email}</td>
-              <td>{photo.phone}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-         <h1>student id 1</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>studentId</th>
-            <th>firstName</th>
-            <th>lastName</th>
-            <th>email</th>
-            <th>phone</th>
-          </tr>
-        </thead>
-        <tbody>
-            <tr key={student.studentId}>
-              <td>{student.studentId}</td>
+          {students.map((student) => (
+            <tr key={student.rollNo}>
+              <td>{student.rollNo}</td>
               <td>{student.firstName}</td>
               <td>{student.lastName}</td>
               <td>{student.email}</td>
-              <td>{student.phone}</td>
+              <td>{student.phoneNumber}</td>
             </tr>
+          ))}
         </tbody>
       </table>
     </div>
